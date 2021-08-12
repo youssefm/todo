3bf8e2c093931d3ef06facb81ea0b17ff576e101
Youssef Moussaoui <yms@outlook.com>
2021-08-11 18:27:27 -0700

    Allow PUTs for completed

--- svelte-web/src/TodoItem.svelte
+++ svelte-web/src/TodoItem.svelte
@@ -2,9 +2,21 @@
   import type { TodoItemData } from "./types";
 
   export let todo: TodoItemData;
+
+  const handleToggle = () => {
+    todo = { ...todo, completed: !todo.completed };
+
+    fetch(`/api/todo/${todo.id}`, {
+      method: "PUT",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
 </script>
 
-<div>
-  <input type="checkbox" bind:checked={todo.completed} />
+<div on:click={handleToggle}>
+  <input type="checkbox" checked={todo.completed} />
   {todo.title}
 </div>
